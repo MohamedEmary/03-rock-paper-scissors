2c878195bcd3dd4ad1a0d2c0176eec28d33b59b1
Mohamed Emary <64380567+MohamedEmary@users.noreply.github.com>
2023-08-30 21:01:10 +0300

    change play and game function names

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
 const choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-	return choices[Math.floor(Math.random() * choices.length)];
+  return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function play(playerSelection, computerSelection) {
-	playerSelection = playerSelection.toLowerCase();
-	let msg;
-	if (computerSelection === playerSelection) {
-		msg = `Draw!\nYou and Computer both chose ${computerSelection}`;
-	} else if (
-		(playerSelection === "rock" && computerSelection === "paper") ||
-		(playerSelection === "paper" && computerSelection === "scissors") ||
-		(playerSelection === "scissors" && computerSelection === "rock")
-	) {
-		msg = `You Lose!\n${computerSelection} beats ${playerSelection}`;
-		return msg;
-	} else {
-		msg = `You Win!\n${playerSelection} beats ${computerSelection}`;
-	}
-	return msg;
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  let msg;
+  if (computerSelection === playerSelection) {
+    msg = `Draw!\nYou and Computer both chose ${computerSelection}`;
+  } else if (
+    (playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "rock")
+  ) {
+    msg = `You Lose!\n${computerSelection} beats ${playerSelection}`;
+    return msg;
+  } else {
+    msg = `You Win!\n${playerSelection} beats ${computerSelection}`;
+  }
+  return msg;
 }
 
-function game() {
-	let computerSelection = getComputerChoice();
-	let playerSelection = prompt("Enter rock paper or scissors");
-	alert(play(playerSelection, computerSelection));
+function playGame() {
+  let computerSelection = getComputerChoice();
+  let playerSelection = prompt("Enter rock paper or scissors");
+  alert(play(playerSelection, computerSelection));
 }
 
 game();
