18c568fd268260d42eca7122243691f12b588fc3
Mohamed Emary <64380567+MohamedEmary@users.noreply.github.com>
2023-08-30 20:28:18 +0300

    add play function

--- js/script.js
+++ js/script.js
@@ -4,3 +4,19 @@ function getComputerChoice() {
 	return Math.floor(Math.random() * choices.length);
 }
 
+function play(playerSelection, computerSelection) {
+	playerSelection = playerSelection.toLowerCase();
+	console.log(`Computer Choice: ${computerSelection}`);
+	console.log(`Player Choice: ${playerSelection}`);
+	if (computerSelection === playerSelection) {
+		return "Draw!";
+	} else if (
+		(playerSelection === "rock" && computerSelection === "paper") ||
+		(playerSelection === "paper" && computerSelection === "scissors") ||
+		(playerSelection === "scissors" && computerSelection === "rock")
+	) {
+		return "Computer Win!";
+	} else {
+		return "Player Win!";
+	}
+}
