476240c3bede79e4726a6845b4cabfd783be1053
Mohamed Emary <64380567+MohamedEmary@users.noreply.github.com>
2023-09-18 01:55:18 +0300

    enhance functionality of the game

--- js/script.js
+++ js/script.js
@@ -1,59 +1,65 @@
+let gameOver = false;
 const choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-let history = { win: 0, lose: 0 }; // ["win", "lose"] it's better to be a dictionary
+let scoreHistory = { playerScore: 0, computerScore: 0 };
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
   let msg;
+  let computerSelection = getComputerChoice();
   if (computerSelection === playerSelection) {
-    msg = `Draw!\nYou and Computer both chose ${computerSelection}`;
+    msg = `Draw!<br>You and Computer both chose ${computerSelection}`;
   } else if (
     (playerSelection === "rock" && computerSelection === "paper") ||
     (playerSelection === "paper" && computerSelection === "scissors") ||
     (playerSelection === "scissors" && computerSelection === "rock")
   ) {
-    msg = `You Lost the round!\n${computerSelection} beats ${playerSelection}`;
-    history.lose++;
+    msg = `You Lost the round!<br>${computerSelection} beats ${playerSelection}`;
+    scoreHistory.computerScore++;
   } else {
-    msg = `You Win the round!\n${playerSelection} beats ${computerSelection}`;
-    history.win++;
+    msg = `You Win the round!<br>${playerSelection} beats ${computerSelection}`;
+    scoreHistory.playerScore++;
   }
-  return msg;
+  if (scoreHistory.playerScore === 5 || scoreHistory.computerScore === 5) {
+    gameOver = true;
+  }
+  writeResultDiv(msg);
 }
 
 function writeResultDiv(resultMsg) {
   const resultDiv = document.querySelector("#result");
-  resultDiv.textContent = resultMsg;
-}
-
-function showFinalResult() {
-  if (history["win"] === 5) {
-    writeResultDiv("You won the game");
-  } else if (history["lose"] === 5) {
-    writeResultDiv("You lost the game");
+  resultDiv.innerHTML = resultMsg;
+  resultDiv.innerHTML += `<br>Player Score: ${scoreHistory.playerScore}<br>`;
+  resultDiv.innerHTML += `Computer Score: ${scoreHistory.computerScore}<br>`;
+  if (gameOver) {
+    resultDiv.innerHTML +=
+      scoreHistory.playerScore > scoreHistory.computerScore
+        ? `Player Win The Game`
+        : `Computer Win The Game`;
+    scoreHistory = { playerScore: 0, computerScore: 0 };
+    gameOver = false;
   }
 }
 
-function addButtonFunctionality(choice) {
-  let resultMsg = playRound(choice, getComputerChoice());
-  writeResultDiv(resultMsg);
+function handleClick(choice) {
+  playRound(choice);
   showFinalResult();
 }
 
 const rockButton = document.querySelector("#rock");
 rockButton.addEventListener("click", function () {
-  addButtonFunctionality("rock");
+  handleClick("rock");
 });
 
 const paperButton = document.querySelector("#paper");
 paperButton.addEventListener("click", function () {
-  addButtonFunctionality("paper");
+  handleClick("paper");
 });
 
 const scissorsButton = document.querySelector("#scissors");
 scissorsButton.addEventListener("click", function () {
-  addButtonFunctionality("scissors");
+  handleClick("scissors");
 });
